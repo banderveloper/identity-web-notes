a20dfdd37ca3a047a0242c176f86b1b03fe4feb8
kpssnik <kpssdeveloper@gmail.com>
2022-12-10 20:32:08 +0200

    backend application & persistence upd / Created classes for group service adding for dependencyInjection (AddPersisrence, AddApplication)

---
+++ IdentityWebNotes.Backend/IdentityWebNotes.Persistence/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using IdentityWebNotes.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IdentityWebNotes.Persistence;
+
+public static class DependencyInjection
+{
+    // Extension method for service collection, adding everything needed from persistence layer
+    public static IServiceCollection AddPersistence(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Injecting db context
+        var connectionString = configuration.GetConnectionString("Sqlite");
+        services.AddDbContext<NotesDbContext>(options =>
+        {
+            options.UseSqlite(connectionString);
+        });
+
+        // Injecting scope for dbContext, implementation gets from service because...
+        // ...it is already injected before (AddDbContext)
+        // AddScoped<INotesDbContext, NotesDbContext> will create second instance of NotesDbContext
+        services.AddScoped<INotesDbContext>(provider =>
+            provider.GetService<NotesDbContext>());
+        
+        return services;
+    }
+}
