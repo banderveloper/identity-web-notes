900a7218b7f675c8d2dc36d67fa25fba38071ecf
kpssnik <kpssdeveloper@gmail.com>
2022-12-10 16:27:13 +0200

    backend persistence upd / Created class for database initialize

---
+++ IdentityWebNotes.Backend/IdentityWebNotes.Persistence/DbInitializer.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityWebNotes.Persistence;
+
+public static class DbInitializer
+{
+    public static void Initialize(NotesDbContext context)
+    {
+        context.Database.EnsureCreated();
+    }
+}
