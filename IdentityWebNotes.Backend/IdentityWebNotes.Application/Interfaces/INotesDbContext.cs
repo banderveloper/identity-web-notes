eb54d97a6bcade12caa776e0952c1d6491821a66
kpssnik <kpssdeveloper@gmail.com>
2022-12-10 16:23:36 +0200

    backend application & persistence upd / Created db context interface and implementation

---
+++ IdentityWebNotes.Backend/IdentityWebNotes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IdentityWebNotes.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityWebNotes.Application.Interfaces;
+
+// Interface for main db context
+// Application (mediatR in our case) will use only interface for communicating with db
+// So we need at least Notes DbSet and SaveChanges function for making changes in db
+// Implementation NotesDbContext created at Persistence level and injected to mediatR by DI
+public interface INotesDbContext
+{
+    DbSet<Note> Notes { get; set; }
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ IdentityWebNotes.Backend/IdentityWebNotes.Persistence/NotesDbContext.cs
@@ -0,0 +1,22 @@
+﻿using IdentityWebNotes.Application.Interfaces;
+using IdentityWebNotes.Domain;
+using IdentityWebNotes.Persistence.EntityTypeConfigurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityWebNotes.Persistence;
+
+public class NotesDbContext : DbContext, INotesDbContext
+{
+    // From INotesDbContext
+    public DbSet<Note> Notes { get; set; }
+
+    // options are delivered to constructor for scoped DI and initializing connection string
+    public NotesDbContext(DbContextOptions<NotesDbContext> options) 
+        : base(options) {}
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // NoteConfiguration - our entity type configuration
+        modelBuilder.ApplyConfiguration(new NoteConfiguration());
+    }
+}
