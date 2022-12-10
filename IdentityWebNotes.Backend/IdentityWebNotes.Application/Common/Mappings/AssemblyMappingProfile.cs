a09fe7f7724c33146066b5e3075bc4cf3dd542b3
kpssnik <kpssdeveloper@gmail.com>
2022-12-10 18:27:51 +0200

    backend application upd / Created interface IMapWith for automapping, and AssemblyMappingProfile, that runs above all classes(DTOs) than implements IMapWith, and class Mapping method for applying default/custom mapping profile

---
+++ IdentityWebNotes.Backend/IdentityWebNotes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using AutoMapper;
+
+namespace IdentityWebNotes.Application.Common.Mappings;
+
+public class AssemblyMappingProfile : Profile
+{
+    public AssemblyMappingProfile(Assembly assembly)
+        => ApplyMappingsFromAssembly(assembly);
+
+    // Method invoked at MAIN, when server starts, applies mapping profiles for each type
+    private void ApplyMappingsFromAssembly(Assembly assembly)
+    {
+        // Get all classes than implements IMapWith interface
+        var iMapWithImplementers = assembly.GetExportedTypes()
+            .Where(type => type.GetInterfaces()
+                .Any(i => i.IsGenericType &&
+                          i.GetGenericTypeDefinition() == typeof(IMapWith<>)))
+            .ToList();
+
+        // run above each class that implements IMapWith interface
+        foreach (var iMapWithImplementer in iMapWithImplementers)
+        {
+            // locally creates instance of class than inherits from IMapWith
+            var instance = Activator.CreateInstance(iMapWithImplementer);
+
+            // gets CUSTOM Mapping method realisation, or DEFAULT realisation described in class-implementer
+            var method = iMapWithImplementer.GetMethod("Mapping");
+
+            // invokes method Mapping at given instance at pass AssemblyMappingProfile there
+            method?.Invoke(instance, new object?[] { this });
+        }
+    }
+}
---
+++ IdentityWebNotes.Backend/IdentityWebNotes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+namespace IdentityWebNotes.Application.Common.Mappings;
+
+// Interface created for auto mapping, each DTO inherits from this interface
+// Method ApplyMappingsFromAssembly from AssemblyMappingProfile
+// using reflections finds all classes than inherits this interface and call Mapping method in them
+
+// this makes it unnecessary to write a large list of mapping profiles.
+// Each dto will describe its own mapping config, and you won't have to look for it.
+public interface IMapWith<T>
+{
+    // Method which calls in every instance inherited from IMapWith (calls by assemblyMappingProfile)
+    // This is a DEFAULT implementation, but it can be custom inside class-implementer
+    void Mapping(Profile profile)
+    {
+        // create DEFAULT map config for _mapper.Map
+        profile.CreateMap(typeof(T), GetType());
+    }
+}
