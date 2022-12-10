dc1c1653fbc16f9bdb566160b6439a76a14de9f6
kpssnik <kpssdeveloper@gmail.com>
2022-12-10 17:40:15 +0200

    backend application upd / Created cqrs delete note command and handler. Commands folder are done!

---
+++ IdentityWebNotes.Backend/IdentityWebNotes.Application/Notes/Commands/DeleteNote/DeleteNoteCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace IdentityWebNotes.Application.Notes.Commands.DeleteNote;
+
+// CQRS command for DeleteNoteCommandHandler
+public class DeleteNoteCommand : IRequest
+{
+    public Guid UserId { get; set; }
+    public Guid Id { get; set; }
+}
---
+++ IdentityWebNotes.Backend/IdentityWebNotes.Application/Notes/Commands/DeleteNote/DeleteNoteCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using IdentityWebNotes.Application.Common.Exceptions;
+using IdentityWebNotes.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityWebNotes.Application.Notes.Commands.DeleteNote;
+
+// CQRS handler for deleting existing note
+public class DeleteNoteCommandHandler : IRequestHandler<DeleteNoteCommand>
+{
+    private readonly INotesDbContext _context;
+
+    public DeleteNoteCommandHandler(INotesDbContext context)
+        => _context = context;
+    
+    
+    // Accepts request than contains user and note id
+    // And deletes note with given id if note exists and userid equals
+    // Throws NotFoundException if note not found or user id not equals
+    public async Task<Unit> Handle(DeleteNoteCommand request, CancellationToken cancellationToken)
+    {
+        // Gets note entity from db
+        var noteEntity = await _context.Notes.FirstOrDefaultAsync(
+            note => note.Id == request.Id, cancellationToken);
+
+        if (noteEntity is null || noteEntity.UserId != request.UserId)
+            throw new NotFoundException(nameof(noteEntity), request.Id);
+        
+        // If entity found and userId equals - deleting note
+        _context.Notes.Remove(noteEntity);
+        await _context.SaveChangesAsync(cancellationToken);
+        
+        return Unit.Value;
+    }
+}
