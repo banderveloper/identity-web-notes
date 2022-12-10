af6651fe027752cf80e4c0a4d164073c00818ca6
kpssnik <kpssdeveloper@gmail.com>
2022-12-10 15:56:26 +0200

    backend upd / Created initial onion architecture, core layer contains domain block for enterprise logic (note entity), application block for bussiness logic, infrastructure->persistence for database handling, presentation->webapi for webapi

---
+++ IdentityWebNotes.Backend/IdentityWebNotes.Domain/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace IdentityWebNotes.Domain;
+
+public class Note
+{
+    public Guid UserId { get; set; }
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Details { get; set; } = string.Empty;
+    public DateTime CreationDate { get; set; }
+    public DateTime? EditDate { get; set; }
+}
