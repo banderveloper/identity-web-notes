eeb8087e6c38eb375cd249d8aa174dcf82c57ce9
kpssnik <kpssdeveloper@gmail.com>
2022-12-10 17:07:05 +0200

    backend application upd / Started Mediatr+CQRS implementation. Created CQRS command for creating note, and handler, than accepts this command and returns GUID of created note

---
+++ IdentityWebNotes.Backend/IdentityWebNotes.Application/Notes/Commands/CreateNote/CreateNoteCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace IdentityWebNotes.Application.Notes.Commands.CreateNote;
+
+// CQRS command, used for CreateNoteCommandHandler, returns Guid of created note
+public class CreateNoteCommand : IRequest<Guid>
+{
+    public Guid UserId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Details { get; set; } = string.Empty;
+}
---
+++ IdentityWebNotes.Backend/IdentityWebNotes.Application/Notes/Commands/CreateNote/CreateNoteCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using IdentityWebNotes.Application.Interfaces;
+using IdentityWebNotes.Domain;
+using MediatR;
+
+namespace IdentityWebNotes.Application.Notes.Commands.CreateNote;
+
+// CQRS handler for creating new note
+public class CreateNoteCommandHandler : IRequestHandler<CreateNoteCommand, Guid>
+{
+    private readonly INotesDbContext _context;
+
+    public CreateNoteCommandHandler(INotesDbContext context)
+    {
+        _context = context;
+    }
+
+    // Creates new note. Accepts create note command with user id, title and details
+    // returns Guid of new created note
+    public async Task<Guid> Handle(CreateNoteCommand request, CancellationToken cancellationToken)
+    {
+        // Creating Note entity from request
+        var note = new Note
+        {
+            UserId = request.UserId,
+            Title = request.Title,
+            Details = request.Details,
+
+            Id = Guid.NewGuid(),
+            CreationDate = DateTime.Now,
+            EditDate = null
+        };
+
+        // adding to db
+        _context.Notes.Add(note);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return note.Id;
+    }
+}
