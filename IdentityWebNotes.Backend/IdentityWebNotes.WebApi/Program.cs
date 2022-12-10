70a8c820b190f6c547b2281cfe4178b121c5cd5b
kpssnik <kpssdeveloper@gmail.com>
2022-12-10 20:51:19 +0200

    backend webapi upd / Injected application&persistence layer, automapper with configs, and dbContext passed to DbInitializer

--- IdentityWebNotes.Backend/IdentityWebNotes.WebApi/Program.cs
+++ IdentityWebNotes.Backend/IdentityWebNotes.WebApi/Program.cs
@@ -1,6 +1,33 @@
+using System.Reflection;
+using IdentityWebNotes.Application;
+using IdentityWebNotes.Application.Common.Mappings;
+using IdentityWebNotes.Application.Interfaces;
+using IdentityWebNotes.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Injection of application and persistence layer groups
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+
+// Injecting automapper configuration for automapping through IMapWith<>
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+});
+
+// Getting NotesDbContext from services, add pass it to DbInitializer
+try
+{
+    var scope = builder.Services.BuildServiceProvider().CreateScope();
+    var context = scope.ServiceProvider.GetService<NotesDbContext>();
+    DbInitializer.Initialize(context);
+}
+catch (Exception ex)
+{
+}
+
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
 app.Run();
