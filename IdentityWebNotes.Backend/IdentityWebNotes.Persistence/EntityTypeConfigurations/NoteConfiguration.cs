d03cdb4a68e9b8d04916ff9c8b8a52e1e1b17cff
kpssnik <kpssdeveloper@gmail.com>
2022-12-10 16:09:17 +0200

    backend persistence upd / Created EntityTypeConfigurations folder for fluentAPI entity configurations, created class NoteConfiguration

---
+++ IdentityWebNotes.Backend/IdentityWebNotes.Persistence/EntityTypeConfigurations/NoteConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using IdentityWebNotes.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IdentityWebNotes.Persistence.EntityTypeConfigurations;
+
+// FluentAPI entity configuration class
+// used in OnModelCreating -> modelBuilder.ApplyConfiguration(...)
+public class NoteConfiguration : IEntityTypeConfiguration<Note>
+{
+    public void Configure(EntityTypeBuilder<Note> builder)
+    {
+        builder.HasKey(note => note.Id);
+        builder.HasIndex(note => note.Id).IsUnique();
+        builder.Property(note => note.Title).HasMaxLength(250);
+    }
+}
