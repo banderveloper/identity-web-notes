6668a1196682fc08e3b260020587352c954764e9
kpssnik <kpssdeveloper@gmail.com>
2022-12-11 19:47:50 +0200

    identity upd / Created db initalizer, context injected to pipeline

---
+++ IdentityWebNotes.Identity/IdentityWebNotes.Identity/Data/DbInitializer.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityWebNotes.Identity.Data;
+
+public static class DbInitializer
+{
+    public static void Initialize(AuthDbContext context)
+    {
+        context.Database.EnsureCreated();
+    }
+}
--- IdentityWebNotes.Identity/IdentityWebNotes.Identity/Program.cs
+++ IdentityWebNotes.Identity/IdentityWebNotes.Identity/Program.cs
@@ -1,17 +1,55 @@
 using IdentityWebNotes.Identity;
+using IdentityWebNotes.Identity.Data;
+using IdentityWebNotes.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder();
 
+// Inject identity db context
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite")));
 
+// Adds and configures the identity system for the specified User and Role types.
+builder.Services.AddIdentity<AppUser, IdentityRole>(config =>
+{
+    config.Password.RequiredLength = 4;
+    config.Password.RequireDigit = false;
+    config.Password.RequireNonAlphanumeric = false;
+    config.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
 
 // Inject custom identity configuration
 builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<AppUser>()
     .AddInMemoryApiResources(Configuration.ApiResources)
     .AddInMemoryIdentityResources(Configuration.IdentityResources)
     .AddInMemoryApiScopes(Configuration.ApiScopes)
     .AddInMemoryClients(Configuration.Clients)
     .AddDeveloperSigningCredential();
 
+// Configures the application cookie.
+builder.Services.ConfigureApplicationCookie(config =>
+{
+    config.Cookie.Name = "Notes.Identity.Cookie";
+    config.LoginPath = "/Auth/Login";
+    config.LogoutPath = "/Auth/Logout";
+});
+
+// Initialize db via DbInitializer, or log error
+var scope = builder.Services.BuildServiceProvider().CreateScope();
+try
+{
+    var context = scope.ServiceProvider.GetService<AuthDbContext>();
+    DbInitializer.Initialize(context);
+}
+catch (Exception ex)
+{
+    var logger = scope.ServiceProvider.GetService<ILogger<Program>>();
+    logger?.LogError(ex, "An error occured while app initialization");
+}
 
 var app = builder.Build();
 
