808eaddab77b8cb3da48b2069b8a653f3329d834
kpssnik <kpssdeveloper@gmail.com>
2022-12-11 12:39:58 +0200

    indentity upd / Injected default service and middleware for testing, it is alive!

--- IdentityWebNotes.Identity/IdentityWebNotes.Identity/Program.cs
+++ IdentityWebNotes.Identity/IdentityWebNotes.Identity/Program.cs
@@ -1,10 +1,17 @@
+using IdentityServer4.Models;
+
 var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryApiResources(new List<ApiResource>())
+    .AddInMemoryIdentityResources(new List<IdentityResource>())
+    .AddInMemoryApiScopes(new List<ApiScope>())
+    .AddInMemoryClients(new List<Client>())
+    .AddDeveloperSigningCredential();
+
+
 var app = builder.Build();
 
-
-
-
-
-
+app.UseIdentityServer();
 
 app.Run();
